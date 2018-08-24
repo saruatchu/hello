2709852d7fd3d72c2aa87f5070c3beea66f94007
saruatchu <sarumathiayyappan@gmail.com>
2018-08-24 11:47:39 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+# hello
+import java.io.*;
+class Hello
+{
+public static void main(String args[])
+int i,n;
+System.out.println("enter the n value"+n);
+if(i=1;i<=n;i++)
+Systrm.out.println("hello"+n);
+}
